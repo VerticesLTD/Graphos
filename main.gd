5278bda3637cea369ed15ecb021a200480d2b213
Elad Cohen <cohenelad08@gmail.com>
2025-12-15 19:57:34 +0200

    Added a show in action inside main that gpt created, currectly without visualizations or right names for the colors(the print is jibrish)

--- main.gd
+++ main.gd
@@ -26,8 +26,33 @@ var CIRCLES = [
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-			
+	test_bfs()
+
+
+# In Main.gd
+func test_bfs():
+	var graph = UndirectedGraph.new()
+
+	# 1. Create 5 vertices
+	for i in range(1, 6):
+		graph.add_vertex(i)
+
+	# 2. Create connections
+	graph.add_edge(1, 2)
+	graph.add_edge(1, 3)
+	graph.add_edge(2, 4)
+	graph.add_edge(3, 5)
+
+	# 3. Run the algorithm
+	var bfs = BFS.new(graph)
+	var start_node = graph.get_vertex(1)
+	
+	if start_node:
+		var tape = bfs.run(start_node)
+		
+		# 4. Print the array as is
+		print(tape)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	# Every frame we ask the engine to redraw.
