73ab1536d5f933159eb00ec2f9cbcb802da1ec4e
YotamAshman <tanihu2@gmail.com>
2026-01-23 12:27:10 +0200

    Create animation for credits

--- boot.gd
+++ boot.gd
@@ -1,7 +1,10 @@
 extends Node
 
+const MAIN = preload("uid://yxcr5kwlindq")
+const CREDITS = preload("uid://c1t7e0pf81xoe")
+
 func _ready() -> void:
 	if OS.has_feature("editor"):
-		get_tree().change_scene_to_file("res://main.tscn")
+		get_tree().change_scene_to_packed.call_deferred(MAIN)
 	else:
-		get_tree().change_scene_to_file("res://ui/credit_scene/credits.tscn")
+		get_tree().change_scene_to_packed.call_deferred(CREDITS)
