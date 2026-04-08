c7df08372aad84e76233e0810d0479f3a88ca6da
Yotam Ashman <tanihu2@gmail.com>
2026-04-08 12:24:47 +0300

    Increase scale factor on mobile

---
+++ scenes/main/resolution_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _is_mobile_web() -> bool:
+	return OS.has_feature("web_android") or OS.has_feature("web_ios")
+
+func _adjust_ui_scale() -> void:
+	if _is_mobile_web():
+		get_window().content_scale_factor = 1.5
